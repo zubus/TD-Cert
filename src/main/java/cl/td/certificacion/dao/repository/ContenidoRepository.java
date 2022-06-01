67e8fc54e6357974d434615fac2c75d8de79b9da
Cristian <cristian@nyumba.cl>
2022-05-31 21:03:23 -0400

    Add repository

--- src/main/java/cl/td/certificacion/dao/repository/ContenidoRepository.java
+++ src/main/java/cl/td/certificacion/dao/repository/ContenidoRepository.java
@@ -1,5 +1,9 @@
 package cl.td.certificacion.dao.repository;
 
-public interface ContenidoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContenidoRepository extends JpaRepository<ContenidoRepository, Integer>{
 
 }
--- src/main/java/cl/td/certificacion/dao/repository/CursoRepository.java
+++ src/main/java/cl/td/certificacion/dao/repository/CursoRepository.java
@@ -1,5 +1,11 @@
 package cl.td.certificacion.dao.repository;
 
-public interface CursoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cl.td.certificacion.dao.entity.Curso;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Integer>{
 
 }
