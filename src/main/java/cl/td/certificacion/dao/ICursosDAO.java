76bae0fd1deb648bee24b0bcc30319178b1b3cd9
Cristian <cristian@nyumba.cl>
2022-05-31 22:39:27 -0400

    dao impl

--- src/main/java/cl/td/certificacion/dao/ICursosDAO.java
+++ src/main/java/cl/td/certificacion/dao/ICursosDAO.java
@@ -1,5 +1,19 @@
 package cl.td.certificacion.dao;
 
+import java.util.List;
+
+import cl.td.certificacion.dao.DTO.ContenidoDTO;
+import cl.td.certificacion.dao.DTO.CursoDTO;
+
 public interface ICursosDAO {
 
+	public List<CursoDTO> obtenerCursos();
+	public CursoDTO obtenerCurso(Integer id);
+	public boolean guardarCurso(CursoDTO curso);
+	public boolean actualizarCurso(CursoDTO curso);
+	public boolean eliminarCurso(Integer id);
+	public int obtenerCupos(Integer id);
+	public void guardarContenido(ContenidoDTO contenido);
+	public boolean actualizarContenido(ContenidoDTO contenido);
+	public boolean eliminarContenido(Integer id);
 }
