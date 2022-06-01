182a9d8caf8895c798c7bd6ba2aeaf8a5a84139e
Cristian <cristian@nyumba.cl>
2022-05-31 20:36:42 -0400

    DTO

--- src/main/java/cl/td/certificacion/dao/DTO/ContenidoDTO.java
+++ src/main/java/cl/td/certificacion/dao/DTO/ContenidoDTO.java
@@ -1,5 +1,45 @@
 package cl.td.certificacion.dao.DTO;
 
-public class ContenidoDTO {
+import java.io.Serializable;
 
+public class ContenidoDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nombre;
+	private String detalle;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDetalle() {
+		return detalle;
+	}
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	public ContenidoDTO(Integer id, String nombre, String detalle) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.detalle = detalle;
+	}
+	public ContenidoDTO(String nombre, String detalle) {
+		super();
+		this.nombre = nombre;
+		this.detalle = detalle;
+	}
+	public ContenidoDTO() {
+		super();
+	}
+	
 }
--- src/main/java/cl/td/certificacion/dao/DTO/CursoDTO.java
+++ src/main/java/cl/td/certificacion/dao/DTO/CursoDTO.java
@@ -1,5 +1,83 @@
 package cl.td.certificacion.dao.DTO;
 
-public class CursoDTO {
+import java.io.Serializable;
+import java.util.Date;
+
+public class CursoDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nombre;
+	private String imagen;
+	private Date fec_inicio;
+	private Date fec_termino;
+	private Integer cupos;
+	private String descripcion;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public Date getFec_inicio() {
+		return fec_inicio;
+	}
+	public void setFec_inicio(Date fec_inicio) {
+		this.fec_inicio = fec_inicio;
+	}
+	public Date getFec_termino() {
+		return fec_termino;
+	}
+	public void setFec_termino(Date fec_termino) {
+		this.fec_termino = fec_termino;
+	}
+	public Integer getCupos() {
+		return cupos;
+	}
+	public void setCupos(Integer cupos) {
+		this.cupos = cupos;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public CursoDTO(Integer id, String nombre, String imagen, Date fec_inicio, Date fec_termino, Integer cupos,
+			String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.imagen = imagen;
+		this.fec_inicio = fec_inicio;
+		this.fec_termino = fec_termino;
+		this.cupos = cupos;
+		this.descripcion = descripcion;
+	}
+	public CursoDTO(String nombre, String imagen, Date fec_inicio, Date fec_termino, Integer cupos,
+			String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.imagen = imagen;
+		this.fec_inicio = fec_inicio;
+		this.fec_termino = fec_termino;
+		this.cupos = cupos;
+		this.descripcion = descripcion;
+	}
+	public CursoDTO() {
+		super();
+	}
 
 }
